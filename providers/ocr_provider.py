808ab649eb5cdf771193e0a1f52d01c68ceac7ce
Сальников Александр <sasha.salnikov.06@mail.ru>
2026-04-15 01:10:19 +0300

    [feat] Add Dxcam instead of mss

--- providers/ocr_provider.py
+++ providers/ocr_provider.py
@@ -1,26 +1,27 @@
 import numpy as np
-import cv2
-import base64
-import mss
+import dxcam
 import asyncio
+import logging
+
+logger = logging.getLogger(__name__)
 
 class OCRProvider:
     def __init__(self) -> None:
-        print("OCR готов")
+        logger.info("Инициализация DXcam...")
+        try:
+            self.camera = dxcam.create(output_color="RGB")
+        except Exception as e:
+            logger.error(f"Ошибка инициализации DXcam: {e}")
+            raise RuntimeError("DXcam недоступен.")
+            
+        logger.info("OCR (Screen Capture) готов")
 
-    async def get_screen(self) -> str:
+    async def get_screen(self) -> np.ndarray:
         return await asyncio.to_thread(self._get_screen_sync)
     
-    def _get_screen_sync(self) -> str:
-        with mss.mss() as sct:
-            screenshot = sct.grab(sct.monitors[1])
-            img = np.array(screenshot)
-            img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
-        
-        _, buffer = cv2.imencode(
-            '.jpg',
-            img,
-            [cv2.IMWRITE_JPEG_QUALITY, 88]
-        )
-        print("OCR: сделал снимок")
-        return base64.b64encode(buffer.tobytes()).decode('utf-8')
+    def _get_screen_sync(self) -> np.ndarray:
+        frame = self.camera.grab()
+        while frame is None:
+            frame = self.camera.grab()
+        logger.debug("OCR: сделал снимок")
+        return frame
