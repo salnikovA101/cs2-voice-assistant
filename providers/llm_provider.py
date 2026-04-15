01d0a54cc1af920038724f1a02fc1d1eceedd71c
Сальников Александр <sasha.salnikov.06@mail.ru>
2026-04-16 01:27:47 +0300

    [feat]: Add chat history

--- providers/llm_provider.py
+++ providers/llm_provider.py
@@ -1,7 +1,8 @@
 import asyncio
 import time
 import logging
-from typing import Any, Dict
+from typing import Any, Dict, Deque
+from collections import deque
 
 from google import genai
 from google.genai.types import GenerateContentConfig, Part, ThinkingConfig, ThinkingLevel
@@ -14,6 +15,7 @@ class LLMProvider:
         self.model_name = self.config["model"]
         self.system_prompt = self._load_prompt(config)
         self.client = genai.Client(api_key=self.config["api_key"])
+        self.history: Deque[Dict[str, str]] = deque(maxlen=self.config.get("history_len", 4))
         logger.info(f"Gemini {self.model_name} готов")
 
     def _load_prompt(self, config: Dict[str, Any]) -> str:
@@ -33,7 +35,7 @@ class LLMProvider:
     async def generate_response(self, user_text: str, image_bytes: bytes) -> str:
         try:
             start = time.perf_counter()
-            
+            print(self.history)
             config = GenerateContentConfig(
                 system_instruction=self.system_prompt,
                 temperature=self.config.get("temperature", 0.9),
@@ -41,14 +43,24 @@ class LLMProvider:
                 thinking_config=ThinkingConfig(thinking_level="high")
             )
 
+            contents = []
+
+            for elem in self.history:
+                contents.append(f"User: {elem['q']}")
+                contents.append(f"Assistant: {elem['a']}")
+
+            contents.append(user_text)
+            contents.append(Part.from_bytes(data=image_bytes, mime_type="image/jpeg"))
+
             response = await self.client.aio.models.generate_content(
                 model=self.model_name,
-                contents=[user_text, Part.from_bytes(data=image_bytes, mime_type="image/jpeg")],
+                contents=contents,
                 config=config
             )
 
-            logger.info(f"LLM: Gemini ответил за {time.perf_counter() - start:.2f} сек")
             text = response.text or "Gemini вернул пустой ответ."
+            self.history.append({'q': user_text, 'a': text.strip()})
+            logger.info(f"LLM: Gemini ответил за {time.perf_counter() - start:.2f} сек")
             return text.strip()
 
         except Exception as e:
